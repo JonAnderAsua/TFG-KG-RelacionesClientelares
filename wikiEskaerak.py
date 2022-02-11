62bb6c7be9edb7f4fa78d74a7ec2f569f5d6ce38
jonander <jonanderasua@gmail.com>
2022-02-11 09:53:50 +0100

    Wikidata eskaera egiten du, falta da jakitea nola atera datuak eskaeratik

--- wikiEskaerak.py
+++ wikiEskaerak.py
@@ -1,18 +1,38 @@
 from qwikidata.sparql import (get_subclasses_of_item, return_sparql_query_results)
+import requests
 
 query = '''
-SELECT ?item ?itemLabel
+SELECT ?person
 WHERE
 {
-    ?item wdt:P31 wd:Q116.
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], esp". }
-
+    ?person wdt:P31 wd:Q5 .
+    ?person rdfs:label "Juan Carlos I de España"@es .
 }
 '''
 
 res = return_sparql_query_results(query)
-print(res.values())
 
-Q_RIVER = "Q4022"
+#https://stackoverflow.com/questions/16228248/how-can-i-get-list-of-values-from-dict
+link = list(res.values())[1]["bindings"][0]["person"]["value"]
+
+# sending get request and saving the response as response object
+r = requests.get(url=link)
+
+# extracting data in json format
+data = r.json()
+print(data["entities"]["Q19943"]["pageid"])
+
+wikiLink = "https://en.wikipedia.org/w/api.php?action=query&pageids="+str(data["entities"]["Q19943"]["pageid"])+"&format=json&formatversion=2"
+
+r = requests.get(url=wikiLink)
+
+# extracting data in json format
+data = r.json()
+print(data)
+
+
+'''
+Q_RIVER = "Q19943"
 subclasses_of_river = get_subclasses_of_item(Q_RIVER)
 print(subclasses_of_river)
+'''
