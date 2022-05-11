2d140bb547ac19a7d34e7bbeb73297b95ba682ed
Jon Ander Asua <jasua@coninpe.es>
2022-05-11 11:23:29 +0200

    Froga gehiago gehiago gehiago

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "graph_test",
+    version = "0.0.1",
+    author = "Jon Ander Asua",
+    author_email = "jasuamiranda1998@gmail.com",
+    description = (""),
+    license = "",
+    keywords = "",
+    packages=['procesSource','graphSource'],
+    package_dir={"": "src"},
+    long_description="Imagine a long description",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        "pytest>=6.1.0"
+    ]
+)
