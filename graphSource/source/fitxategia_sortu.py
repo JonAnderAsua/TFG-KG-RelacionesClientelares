a1600813aa5a8e60183a79467e7211743ac49cd0
Jon Ander Asua <jasua@coninpe.es>
2022-05-18 10:03:30 +0200

    Para sobrescribir el fichero borro su contenido antes con el método ezabatuAurrekoTestua

--- graphSource/source/fitxategia_sortu.py
+++ graphSource/source/fitxategia_sortu.py
@@ -1,14 +1,18 @@
 class Grafo_fitxategia_sortu:
 
     def __init__(self, rdf_output,grafoa):
-
         # Grafoa
         self.grafo = grafoa
 
         #Irteera fitxategia
         self.rdf_output = rdf_output
 
+    def ezabatuAurrekoTestua(self): #https://www.delftstack.com/es/howto/python/python-clear-file/
+        with open(self.rdf_output,'r+') as f:
+            f.truncate(0)
 
     #Main metodoa
     def main(self):
+
+        self.ezabatuAurrekoTestua()
         self.grafo.serialize(destination=self.rdf_output, format="nt")
