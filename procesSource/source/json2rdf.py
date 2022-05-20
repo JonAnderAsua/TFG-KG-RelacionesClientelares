06b544817cdf14a494983dede893a80c29f55b04
jonander <jonanderasua@gmail.com>
2022-05-20 12:13:52 +0200

    Descomentando cosas

--- procesSource/source/json2rdf.py
+++ procesSource/source/json2rdf.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
     programa = grafo_objektua_sortu.Grafo_fitxategia_sortu(procesador.data_source, procesador.logs, procesador.named_graph, procesador.triple_store)
     programa.main()
 
-
     grafoa = programa.getGrafoa()
     fitxategia_programa = fitxategia_sortu.Grafo_fitxategia_sortu(procesador.rdf_output,grafoa)
     fitxategia_programa.main()
@@ -27,7 +26,7 @@ if __name__ == "__main__":
 
     print("Grafoa zerbitzarira igoko da...")
     zerbitzariraIgo = zerbitzarira_igo.Zerbitzarira_igo(procesador.rdf_output, procesador.triple_store, procesador.logs, procesador.delete_graph)
-    # zerbitzariraIgo.zerbitzariraIgo()
+    zerbitzariraIgo.zerbitzariraIgo()
     print("Grafoa " + procesador.triple_store + " helbidera igo da...")
     
     #Aquí habría que añadir lo de SHACL (No es prioritario)
