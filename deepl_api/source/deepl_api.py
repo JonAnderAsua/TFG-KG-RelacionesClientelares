f678c63872453850ac85df8b78983d7a68eda612
jonander <jonanderasua@gmail.com>
2022-07-11 20:21:07 +0200

    Cambiando cosas

--- deepl_api/source/deepl_api.py
+++ deepl_api/source/deepl_api.py
@@ -1,24 +1,15 @@
 import requests
 import json
 
-#APIaren gakoa lortu
-with open('../files/key') as f:
-    for linea in f:
-        key = linea
+class DeeplApi:
+    def __init__(self):
+        #APIaren gakoa lortu
+        with open('deepl_api/files/key') as f:
+            for linea in f:
+                self.key = linea
+        self.uri = 'https://api-free.deepl.com/v2/translate'
 
-#Itzuli behar den testua lortu
-texto_a_traducir = ""
-with open('../files/texto_a_traducir.txt') as f:
-    for linea in f:
-        texto_a_traducir += linea + "\n"
-
-#Itzulpena
-uri = 'https://api-free.deepl.com/v2/translate'
-datuak = {'auth_key':key,'text':texto_a_traducir,'target_lang':'EN'}
-
-res = requests.post(uri, data=datuak)
-
-print('Itzulpena hurrengoa da:')
-print(json.loads(res.text)['translations'][0]['text'])
-
-
+    def itzulpenaEgin(self, testua):
+        datuak = {'auth_key': self.key, 'text': testua, 'target_lang': 'EN'}
+        res = requests.post(self.uri, data=datuak)
+        return json.loads(res.text)['translations'][0]['text']
