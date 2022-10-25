60be133ccc5c971468a14a352b254cc7c6b6b0db
jonander <jonanderasua@gmail.com>
2022-10-25 14:39:21 +0200

    Aldatuta Graphdbren bertsioa

--- README.md
+++ README.md
@@ -17,9 +17,9 @@ README honetan programa hau nola konfiguratzen eta exekutatzen den azaltzen da. 
 ## Erabilitako teknologiak
 - Python >= 3.7
 - Docker 20.10.7
-- GraphDB 9.7.0 Free Edition (deskargatzeko [SAKATU HEMEN](https://www.ontotext.com/products/graphdb/download/))
+- GraphDB 10.0.3 Free Edition (deskargatzeko [SAKATU HEMEN](https://www.ontotext.com/products/graphdb/download/))
 
-Deskargatutako _GraphDB_ binarioa graphDB/ fitxategiaren barnean egon behar da eta hurrengo formatua jarraitu behar du: _graphdb-$edizioa-$bertsioa-dist.zip_. Kasu honetan bitarraren izena _graphdb-free-9.7.0-dist.zip_, beste bat izatekotan, _Dockerfile_ fitxategian parametroak aldatu behar dira.
+Deskargatutako _GraphDB_ binarioa graphDB/ fitxategiaren barnean egon behar da eta hurrengo formatua jarraitu behar du: _graphdb-$edizioa-$bertsioa-dist.zip_. Kasu honetan bitarraren izena _graphdb-free-10.0.3-dist.zip_, beste bat izatekotan, _Dockerfile_ fitxategian parametroak aldatu behar dira.
 
 ## Aurrebaldintzak
 
--- graphSource/source/irudiak_deskargatu.py
+++ graphSource/source/irudiak_deskargatu.py
@@ -9,19 +9,16 @@ class IrudiakDeskargatu:
     def __init__(self, izena):
 
         self.izena = izena
-        self.paths = self.response.download(self.arguments)
-
         self.irudiIzena = self.izena.replace(' ','_')
-
-    def irudiaDeskargatu(self):
         self.response = google_images_download.googleimagesdownload()
         self.arguments = {"keywords": unidecode.unidecode(self.izena), "limit": 1, "print_urls":True}
+
+    def irudiaDeskargatu(self):
         try:
             self.paths = self.response.download(self.arguments)
-
-            print('mv downloads/"' + self.izena + '"/* ' + self.paths + '/' + self.izena+'.jpg')
-            os.system('mv downloads/"' + self.izena + '"/* ' + self.paths + '/' + self.izena+'.jpg')
+            os.system('mv downloads/"' + self.izena + '"/* grafoavis/public/images/' + self.irudiIzena+'.jpg')
             print(self.izena + '(r)en irudia deskargatu da')
 
         except:
-            os.system("googleimagesdownload --k '" + self.izena +"' -l 1")
+            # os.system("googleimagesdownload --k '" + self.izena +"' -l 1")
+            print(self.izena + "-ren irudia ezin izan da deskargatu")
